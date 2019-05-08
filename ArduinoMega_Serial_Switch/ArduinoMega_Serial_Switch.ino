114aa21db9838a0fc2216f474360705988d7fe2d
pswiss <petrasswissler2022@u.northwestern.edu>
2019-05-08 14:06:11 -0500

    Desired code written. Need to Debug.

--- ArduinoMega_Serial_Switch/ArduinoMega_Serial_Switch.ino
+++ ArduinoMega_Serial_Switch/ArduinoMega_Serial_Switch.ino
@@ -8,11 +8,13 @@ Description:  Simple sketch to allow Arduino Mega to
 TODO:         Enable sending RX to the serial ports
               Enable baseline functionailty
 /////////////////////////////////////////////////////////*/
-// Definitions --------------------------------------------
+// Definitions and Variables ------------------------------
 #define BAUD_RATE_COMPUTER_INTERFACE    115200
+int whichPort = '1';
 
 // Code ---------------------------------------------------
-void setup() {
+void setup() 
+{
   // Begin the Arduino <-> PC serial communication
   Serial.begin(BAUD_RATE_COMPUTER_INTERFACE);
   Serial.println("System ON");
@@ -25,7 +27,7 @@ void setup() {
   delay(10);
   deviceBaudRate = Serial.parseInt();
   Serial1.begin(deviceBaudRate);
-  Serial.print(deviceBaudRate);
+  Serial.println(deviceBaudRate);
 
   // Device 2
   Serial.print("Serial baud rate for device 2:\t");
@@ -33,7 +35,7 @@ void setup() {
   delay(10);
   deviceBaudRate = Serial.parseInt();
   Serial2.begin(deviceBaudRate);
-  Serial.print(deviceBaudRate);
+  Serial.println(deviceBaudRate);
 
   // Device 3
   Serial.print("Serial baud rate for device 3:\t");
@@ -41,11 +43,55 @@ void setup() {
   delay(10);
   deviceBaudRate = Serial.parseInt();
   Serial3.begin(deviceBaudRate);
-  Serial.print(deviceBaudRate);
+  Serial.println(deviceBaudRate);
 
+  // Enter main loop
+  Serial.println("Enter Main Loop: Press number corresponding to device to switch which device is reported");
+  delay(1000)
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  // Check for change 
+  if(Serial.available()>0)
+  {
+    whichPort = Serial.read();
+    while(Serial.available()>0)
+    {
+      Serial.read();
+    }
+    Serial.println("-------------------------------------------------------");
+    Serial.print("Switch to device "); Serial.print(whichPort);
+    Serial.println("-------------------------------------------------------");
+  }
+
+  // Report based on which serial port
+  switch(whichPort)
+  {
+    case '1':
+      while(Serial1.available()>0)
+      {
+        Serial.print(Serial1.read());
+      }
+    break;
+
+    case '2':
+      while(Serial2.available()>0)
+      {
+        Serial.print(Serial2.read());
+      }
+    break;
+
+    case '3':
+      while(Serial3.available()>0)
+      {
+        Serial.print(Serial3.read());
+      }
+    break;
+
+    default:
+      Serial.print("Invalid device specified: "); Serial.print(whichPort);
+    break;
+  }
 
 }
